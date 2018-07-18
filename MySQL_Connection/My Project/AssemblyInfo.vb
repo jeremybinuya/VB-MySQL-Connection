ab6c438d9badf66c1703c276b5cf039424370a04
Jeremy Binuya <jeremyrbinuya@gmail.com>
2018-07-18 09:01:43 +0800

    Initial commit

---
+++ MySQL_Connection/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MySQL_Connection")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MySQL_Connection")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b6c938e4-cbe7-4c9d-b5c1-f4ad43633e2f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ MySQL_Connection/frmMySQL_Con.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Configuration
+Imports MySql.Data.MySqlClient
+Public Class frmMySQL_Con
+    Dim MySQLConn As MySqlConnection
+
+    Private Sub btnCheck_Conn_Click(sender As Object, e As EventArgs) Handles btnCheck_Conn.Click
+        MySQLConn = New MySqlConnection
+
+        MySQLConn.ConnectionString = ConfigurationManager.AppSettings("MySQLConn")
+
+        Try
+            MySQLConn.Open()
+            MessageBox.Show("Connection Success")
+            MySQLConn.Clone()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            MySQLConn.Dispose()
+        End Try
+    End Sub
+End Class
